b3953c20b9aef4797e52e01977cdf68528f39e10
yoonshik <yoonshik@gmail.com>
2015-07-19 13:23:19 -0700

    change to console.log

--- webDisplay/webdisplay/static/polling.js
+++ webDisplay/webdisplay/static/polling.js
@@ -1,6 +1,6 @@
 function doPoll(){
     $.post('http://192.168.66.180:6543/check_camera', function(data) {
-        alert(data);
+        console.log(data);
         setTimeout(doPoll,5000);
     });
 }
