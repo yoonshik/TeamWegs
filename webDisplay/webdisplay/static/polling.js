ba299171216992ab4b5b1d2fc0c6643a44df6e1b
yoonshik <yoonshik@gmail.com>
2015-07-19 13:27:09 -0700

    username and password

--- webDisplay/webdisplay/static/polling.js
+++ webDisplay/webdisplay/static/polling.js
@@ -1,5 +1,5 @@
 function doPoll(){
-    $.post('http://192.168.66.180:6543/check_camera', function(data) {
+    $.post('http://192.168.66.180:6543/check_camera', {'username':'yoonshik', 'password':'viasat'}, function(data) {
         console.log(data);
         setTimeout(doPoll,5000);
     });
