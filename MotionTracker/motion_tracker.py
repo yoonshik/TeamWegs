078ae2d1ef57e5c6ac445c9370b8cd1dd920ccc6
William Rory Kronmiller <wrk961@gmail.com>
2015-07-18 22:49:21 -0400

    Wrote wrapper for camera

--- MotionTracker/motion_tracker.py
+++ MotionTracker/motion_tracker.py
@@ -1,21 +1,33 @@
 #!/usr/bin/python2
 import time, cv2
 
-class MotionTracker:
+class CamStreamer:
 
 	def __init__(self):
 		self.CAM_ID = 0
 		self.CAM = None
-		self.THRESH_MIN = 100
-		self.THRESH_MAX = 255
-		self.WAIT_INTERVAL = .005
-		self.NUM_DIFF_THRESHOLD = 80
 
 	def set_cam(self, cam_id):
 		self.CAM_ID = cam_id
 
 	def init_cam(self):
 		self.CAM = cv2.VideoCapture(self.CAM_ID)
+
+	def release_cam(self):
+		self.CAM.release()
+	
+	def read_frame(self):
+		(status, image) = self.CAM.read()
+		return image
+
+class MotionTracker:
+
+	def __init__(self, cam_stream):
+		self.CAM_STREAM = cam_stream
+		self.THRESH_MIN = 100
+		self.THRESH_MAX = 255
+		self.WAIT_INTERVAL = .005
+		self.NUM_DIFF_THRESHOLD = 80
 
 	def _display_img(self, image):
 		cv2.imshow("Test output", image)
@@ -26,17 +38,6 @@ class MotionTracker:
 			return False
 		return True
 
-	def release_cam(self):
-		self.CAM.release()
-
-	#Clear camera buffer
-	def _flush_cam(self):
-		self.CAM.release()
-		self.init_cam()
-		
-		for i in range(0, 100):
-			self.CAM.read()
-
 	def _threshold_img(self, image):
 		
 		(retval, thresh_image) = cv2.threshold(image, self.THRESH_MIN, self.THRESH_MAX, cv2.THRESH_BINARY)
@@ -45,7 +46,7 @@ class MotionTracker:
 	def _diff_image(self, sleep_interval):
 
 		#Get first frame
-		(retval_one, image_one) = self.CAM.read()
+		image_one = self.CAM_STREAM.read_frame()
 
 		#Sleep for specified interval in seconds
 		time.sleep(sleep_interval)
@@ -53,7 +54,7 @@ class MotionTracker:
 		#self._flush_cam();
 		
 		#Get second frame
-		(retval_two, image_two) = self.CAM.read()
+		image_two = self.CAM_STREAM.read_frame()
 
 		#Convert images to grayscale
 		image_one_gray = cv2.cvtColor(image_one, cv2.COLOR_BGR2GRAY)
@@ -88,9 +89,11 @@ class MotionTracker:
 			print(is_motion)
 
 def main():
-	motion_tracker = MotionTracker()
+	
+	cam_stream = CamStreamer()
+	motion_tracker = MotionTracker(cam_stream)
 
-	motion_tracker.init_cam()
+	cam_stream.init_cam()
 	motion_tracker.run(True)
 	motion_tracker.release_cam()
 
