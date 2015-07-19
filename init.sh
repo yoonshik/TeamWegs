73e621a9d0c2f0d9a720022360a552211f571c50
wrkronmiller <wrk961@gmail.com>
2015-07-19 12:55:39 -0700

    Latest and greatest

--- init.sh
+++ init.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-dnf -y install git python python-pip opencv* usbutils vim
-pip install requests
+dnf -y install git python python-pip opencv* usbutils vim python-pyramid npm
+pip install requests pymysql
+npm install -g bower
 dnf -y install mariadb mariadb-server
 echo "About to run the SQL secure installer. Please do not set a password for root. Accept all other defaults"
 mysql_secure_installation
 cd sqlScripts && ./initdb.sh && cd -
+cd webDisplay && python setup.py develop && cd -
 echo "Done"
