b230ce98a9977aaff8a7c445de3924815b8b6647
Yoonshik Hong <yoonshik@yoonshiks-mbp.hq.corp.viasat.com>
2015-07-18 21:17:23 -0700

    handle post request from motion detector

--- webDisplay/webdisplay/views.py
+++ webDisplay/webdisplay/views.py
@@ -4,3 +4,7 @@ from pyramid.view import view_config
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
     return {'project': 'webDisplay'}
+
+@view_config(route_name='motion_detected', renderer='templates/mytemplate.pt')
+def motion_detected_view(request):
+    return {'project':'webDisplay'}
