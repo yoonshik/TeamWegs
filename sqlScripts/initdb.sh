26ede57ebbbeb3f628853b46ad98d54da1d0ecff
William Rory Kronmiller <wrk961@gmail.com>
2015-07-19 12:57:50 -0400

    Doing stuffs

--- sqlScripts/initdb.sh
+++ sqlScripts/initdb.sh
@@ -2,5 +2,5 @@
 echo "Logging into local MySQL/MariaDB root account. For hackathon we assume no password."
 mysql -u root < ./makeDB.sql
 echo "Database created"
-mysql -u root < ./makeUsers.sql
+mysql -u root < ./makeTables.sql
 echo "User table created"
---
+++ sqlScripts/makeTables.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS FoodTracker.Users (
+	uuid int PRIMARY KEY AUTO_INCREMENT,
+	first_name varchar(100) NULL,
+	last_name varchar(250) NULL,
+	username varchar(100) NOT NULL,
+	password varchar(300) NOT NULL
+);
+
+SHOW COLUMNS FROM FoodTracker.Users;
+
+CREATE TABLE IF NOT EXISTS FoodTracker.Events (
+	uuid int PRIMARY KEY AUTO_INCREMENT,
+	time_stamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	is_food char(1) DEFAULT '?'
+);
+
+SHOW COLUMNS FROM FoodTracker.Events;
