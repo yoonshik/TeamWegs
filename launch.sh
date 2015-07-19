eff828970f5161b359e308503494f67834e63d35
wrkronmiller <wrk961@gmail.com>
2015-07-19 13:41:38 -0700

    Things

--- launch.sh
+++ launch.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo "Starting the food tracker of doom!"
 #Launch motion tracker
-screen -S MotionTracker -d -m ./MotionTracker/motion_tracker.py
+screen -SL MotionTracker -d -m ./MotionTracker/motion_tracker.py
 echo "Motion tracker started"
 systemctl start mariadb.service
 systemctl status mariadb.service
 echo "SQL Server activated"
 cd webDisplay
-screen -S pyramidWebDisplay -d -m pserve development.ini
+screen -SL pyramidWebDisplay -d -m pserve development.ini
 cd -
 echo "Wegs is love, Wegs is life."
