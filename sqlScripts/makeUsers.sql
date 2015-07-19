f5c99292a3dbe9d9d5e25a3dfc2cd322129adc54
wrkronmiller <wrk961@gmail.com>
2015-07-19 09:58:14 -0700

    other stuffs

--- sqlScripts/makeUsers.sql
+++ sqlScripts/makeUsers.sql
@@ -2,8 +2,9 @@ CREATE TABLE IF NOT EXISTS FoodTracker.Users (
 	uuid int PRIMARY KEY AUTO_INCREMENT,
 	first_name varchar(100) NULL,
 	last_name varchar(250) NULL,
-	username varchar(100) NOT NULL,
-	password varchar(300) NOT NULL
+	username varchar(100) NOT NULL UNIQUE,
+	password varchar(300) NOT NULL,
+	is_admin BIT(1) NOT NULL DEFAULT 0
 	);
 
 SHOW COLUMNS FROM FoodTracker.Users;
---
+++ sqlScripts/sqlLink.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+import pymysql
+
+class dbLink:
+
+	def __init__(self):
+		self.SQL_CON = pymysql.connect(host='localhost', user='root', db="FoodTracker")
+		print("Connected to SQL Server as root")
+
+	def _run_query(self, query, parameters):
+		cursor = self.SQL_CON.cursor()
+		cursor.execute(query, parameters)
+		self.SQL_CON.commit()
+		return cursor.fetchall()
+
+	def show_users(self):
+		query = "SELECT first_name, last_name, username FROM Users"
+		result = self._run_query(query, [])
+		print("Users:")
+		for (first_name, last_name, username) in result:
+			print(username)
+		print("--------")
+
+	def add_user(self, username, password):
+		query = "INSERT INTO Users (username, password) VALUES (%s, %s)"
+		parameters = [username, password]
+		result = self._run_query(query, parameters)
+
+	def make_admin(self, username):
+		query = "UPDATE Users SET is_admin = 1 WHERE username=%s"
+		parameters = [username]
+		result = self._run_query(query, parameters)
+	
+	def get_admins(self):
+		query = "SELECT * FROM Users WHERE is_admin = 1"
+		result = self._run_query(query, [])
+		print(result)
+def main():
+	db_link = dbLink()
+	db_link.show_users()
+	#db_link.add_user("Rory", "viasat")
+	#db_link.make_admin("Rory")
+	#db_link.add_user("Yoonshik", "ILuvChineseFood")
+	db_link.get_admins()
+
+if __name__ == "__main__":
+	main()
