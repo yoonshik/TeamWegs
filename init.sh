a29eb1996cfeb3b1c28c9c18ba65a1ba61ffbc6d
wrkronmiller <wrk961@gmail.com>
2015-07-19 09:10:13 -0700

    Added dependency

--- init.sh
+++ init.sh
@@ -2,5 +2,7 @@
 dnf -y install git python python-pip opencv* usbutils vim
 pip install requests
 dnf -y install mariadb mariadb-server
+echo "About to run the SQL secure installer. Please do not set a password for root. Accept all other defaults"
+mysql_secure_installation
 cd sqlScripts && ./initdb.sh && cd -
 echo "Done"
