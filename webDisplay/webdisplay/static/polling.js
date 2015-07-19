fbab6e38132424f6c4523c52f4c56a59239d9e71
yoonshik <yoonshik@gmail.com>
2015-07-19 14:23:57 -0700

    add login

--- webDisplay/webdisplay/static/polling.js
+++
@@ -1,7 +0,0 @@
-function doPoll(){
-    payload = JSON.stringify({username:'yoonshik', password:'viasat'})
-    $.post('/check_camera', payload, function(data) {
-        console.log(data);
-        setTimeout(doPoll,5000);
-    });
-}
